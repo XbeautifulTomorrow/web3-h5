dc60a36a884ae7bbcc2671e446fc4398324e1476
Ikaris-a <240570941@qq.com>
2023-04-06 15:44:01 +0800

    3d

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,24 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="bitzing">
+    <div>
+      <img src="./img/logo.svg" alt="" />
+    </div>
+    <div class="bitzing-text">Where Surprises Meet Value!</div>
+    <div
+      class="bitzing-main container"
+      ref="container"
+      @mouseenter="mouseenterFun"
+      @mouseleave="mouseleaveFun"
+      @mousemove="mousemoveFun"
+    >
+      <div class="inner" :style="style" ref="imgElement">
+        <img :style="style" src="./img/main.png" alt="" />
+      </div>
+    </div>
+    <div class="bitzing-link">
+      <img class="bitzing-link-twitter" src="./img/twitter.svg" alt="" />
+      <img src="./img/discord.svg" alt="" />
+    </div>
   </div>
 </template>
 
@@ -34,25 +26,133 @@
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+  data() {
+    return {
+      counter: 0,
+      updateRate: 10,
+      tilt: 2,
+      container: undefined,
+      imgElement: undefined,
+      style: '',
+      mouse: {
+        _x: 0,
+        _y: 0,
+        x: 0,
+        y: 0,
+      },
+    };
+  },
+  created() {
+    // this.test();
+  },
+  mounted() {
+    this.$nextTick(() => {
+      const { container, imgElement } = this.$refs;
+      this.container = container;
+      this.imgElement = imgElement;
+      this.setOrigin(container);
+    });
+  },
+  methods: {
+    mouseenterFun(event) {
+      this.update(event);
+    },
+    mouseleaveFun() {
+      this.style = '';
+    },
+    mousemoveFun(event) {
+      this.counter = this.counter + 1;
+      if (this.isTimeToUpdate()) {
+        this.update(event);
+      }
+    },
+    isTimeToUpdate() {
+      const { counter, updateRate } = this;
+      const isTimeToUpdate = counter % updateRate === 0;
+      return isTimeToUpdate;
+    },
+    updatePosition(event) {
+      const { mouse } = this;
+      const { _x, _y } = mouse;
+      const e = event || window.event;
+      this.mouse.x = e.clientX - _x;
+      this.mouse.y = (e.clientY - _y) * -1;
+    },
+    setOrigin(e) {
+      this.mouse._x = e.offsetLeft + Math.floor(e.offsetWidth / 2);
+      this.mouse._y = e.offsetTop + Math.floor(e.offsetHeight / 2);
+    },
+    show() {
+      const { x, y } = this.mouse;
+      return '(' + x + ', ' + y + ')';
+    },
+    update(event) {
+      const { mouse, imgElement, tilt } = this;
+      this.updatePosition(event);
+      this.updateTransformStyle(
+        ((mouse.y / imgElement.offsetHeight) * tilt).toFixed(2),
+        ((mouse.x / imgElement.offsetWidth) * tilt).toFixed(2)
+      );
+    },
+    updateTransformStyle(x, y) {
+      const style = 'rotateX(' + x + 'deg) rotateY(' + y + 'deg)';
+      this.style = Object.assign({}, { transform: style });
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+/* @import url('./index.scss'); */
+.bitzing {
+  background-image: url('./img/bg.jpg');
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+  width: 100vw;
+  min-height: 100vh;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.bitzing-text {
+  height: 34px;
+  background-image: linear-gradient(
+    to bottom,
+    #02081d 100%,
+    #009afe 71%,
+    #68ddfa 60%,
+    #ede8f7 46%,
+    #2e00a3 46%,
+    #ff2daf 25%,
+    #ff75ca 11%
+  );
+  font-family: Teko;
+  font-size: 24px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  -webkit-background-clip: text;
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.bitzing-link-twitter {
+  margin-right: 80px;
 }
-a {
-  color: #42b983;
+.container {
+  perspective: 30px;
+}
+
+.inner {
+  transition: transform 0.5s;
+}
+
+.container:hover .demo-overlay {
+  opacity: 1;
+}
+
+.container:hover {
+  cursor: pointer;
 }
 </style>

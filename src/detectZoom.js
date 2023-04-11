d1e1af0320bcfd7b9d5ebdb5f8bc15465adcfef9
Ikaris-a <240570941@qq.com>
2023-04-11 17:45:40 +0800

    detectZoom

--- src/detectZoom.js
+++ src/detectZoom.js
@@ -1,21 +1,27 @@
 export const detectZoom = () => {
-    let ratio = 0,
-      screen = window.screen,
-      ua = navigator.userAgent.toLowerCase();
-    if (window.devicePixelRatio !== undefined) {
-      ratio = window.devicePixelRatio;
-    } else if (~ua.indexOf('msie')) {
-      if (screen.deviceXDPI && screen.logicalXDPI) {
-        ratio = screen.deviceXDPI / screen.logicalXDPI;
-      }
-    } else if (
-      window.outerWidth !== undefined &&
-      window.innerWidth !== undefined
-    ) {
-      ratio = window.outerWidth / window.innerWidth;
+  let ratio = 0,
+    screen = window.screen,
+    ua = navigator.userAgent.toLowerCase();
+  if (window.devicePixelRatio !== undefined) {
+    ratio = window.devicePixelRatio;
+  } else if (~ua.indexOf('msie')) {
+    if (screen.deviceXDPI && screen.logicalXDPI) {
+      ratio = screen.deviceXDPI / screen.logicalXDPI;
     }
-    if (ratio) {
-      ratio = Math.round(ratio * 100);
-    }
-    return ratio;
-  };
+  } else if (
+    window.outerWidth !== undefined &&
+    window.innerWidth !== undefined
+  ) {
+    ratio = window.outerWidth / window.innerWidth;
+  }
+  if (ratio) {
+    ratio = Math.round(ratio * 100);
+  }
+  return ratio;
+};
+
+export const scaleFun = () => {
+  let percent = document.documentElement.clientWidth / 1920;
+  const scale = percent > 1 ? percent : 1; //大于1k屏就对应比例放大，小于1k屏就用1k屏
+  return scale;
+};
--- src/main.js
+++ src/main.js
@@ -1,43 +1,44 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import { detectZoom } from './detectZoom.js';
+import {  scaleFun } from './detectZoom.js';
 
-const m = detectZoom();
-//判断屏幕是否为4k
-if (window.screen.width * window.devicePixelRatio >= 3840) {
-  // switch (m) {
-  //   // 4k屏时屏幕缩放比为100%
-  //   case 100:
-  //     document.body.style.zoom = 100 / 50;
-  //     break;
-  //     // 4k屏时屏幕缩放比为125%
-  //   case 125:
-  //     document.body.style.zoom = 100 / 62.5;
-  //     break;
-  //     // 4k屏时屏幕缩放比为150%
-  //   case 150:
-  //     document.body.style.zoom = 100 / 75;
-  //     break;
-  //     // 4k屏时屏幕缩放比为175%
-  //   case 175:
-  //     document.body.style.zoom = 100 / 87.4715;
-  //     break;
-  //     // 4k屏时屏幕缩放比为200%
-  //   case 200:
-  //     document.body.style.zoom = 100 / 100;
-  //     break;
-  //     // 4k屏时屏幕缩放比为225%
-  //   case 225:
-  //     document.body.style.zoom = 100 / 112.485;
-  //     break;
+// const m = detectZoom();
+const s = scaleFun();
+// //判断屏幕是否为4k
+// if (window.screen.width * window.devicePixelRatio >= 3840) {
+//   switch (m) {
+//     // 4k屏时屏幕缩放比为100%
+//     case 100:
+//       document.body.style.zoom = 100 / 50;
+//       break;
+//     // 4k屏时屏幕缩放比为125%
+//     case 125:
+//       document.body.style.zoom = 100 / 62.5;
+//       break;
+//     // 4k屏时屏幕缩放比为150%
+//     case 150:
+//       document.body.style.zoom = 100 / 75;
+//       break;
+//     // 4k屏时屏幕缩放比为175%
+//     case 175:
+//       document.body.style.zoom = 100 / 87.4715;
+//       break;
+//     // 4k屏时屏幕缩放比为200%
+//     case 200:
+//       document.body.style.zoom = 100 / 100;
+//       break;
+//     // 4k屏时屏幕缩放比为225%
+//     case 225:
+//       document.body.style.zoom = 100 / 112.485;
+//       break;
 
-  //   default:
-  //     break;
-  // }
-  //   transform: scale()
-  document.body.style.transform = `scale(${100 / (Number(m) / 2)})`;
-} else {
-  document.body.style.transform = `scale(${100 / Number(m)})`;
-}
+//     default:
+//       document.body.style.transform = `scale(${100 / (Number(m) / 2)})`;
+//       break;
+//   }
+// } else {
+//   document.body.style.transform = `scale(${100 / Number(m)})`;
+// }
+document.body.style.transform = `scale(${s})`;
 
 createApp(App).mount('#app');

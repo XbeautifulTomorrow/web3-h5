563becd6a0e000398d3f266d8eb5476b21b6f45b
Ikaris-a <240570941@qq.com>
2023-04-25 13:47:13 +0800

    move com

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 //1. 定义要使用到的路由组件  （一定要使用文件的全名，得包含文件后缀名）
-import Header from '../components/header/index.vue';
-import Footer from '../components/footer/index.vue';
+import Header from '../views/header/index.vue';
+import Footer from '../views/footer/index.vue';
 
-const welcome = () => import('../components/welcome/index.vue');
-const Home = () => import('../components/home/index.vue');
-const Login = () => import('../components/login/index.vue');
-const Lottery = () => import('../components/lottery/index.vue');
-const BlindBoxDetail = () => import('../views/blindBox/detail.vue');
+const welcome = () => import('../views/welcome/index.vue');
+const Home = () => import('../views/home/index.vue');
+const Login = () => import('../views/login/index.vue');
+const Lottery = () => import('../views/lottery/index.vue');
+const BlindBoxDetail = () => import('../views/blindBox/index.vue');
 
 //2. 路由配置
 const routes = [

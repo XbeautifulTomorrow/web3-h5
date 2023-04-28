d73d0eb2ab58add3a0d394271483ba41ece2391d
Ikaris-a <240570941@qq.com>
2023-04-28 14:52:34 +0800

    login

--- src/store/header.js
+++ src/store/header.js
@@ -6,10 +6,14 @@ export const useHeaderStore = defineStore('headerStore', {
   state: () => ({ balance: '' }),
   actions: {
     async getTheUserBalanceApi(params) {
+      console.log(window.web3);
       const res = await getTheUserBalance(params);
       if (res && res.data) {
         this.balance = res.data[0].balance;
       }
+    },
+    setBalance(data) {
+      this.balance = data;
     },
   },
 });
--- src/views/header/index.vue
+++ src/views/header/index.vue
@@ -102,6 +102,7 @@ import {
 
 import WalletList from '../login/index.vue';
 import { BigNumber } from 'bignumber.js';
+
 export default {
   name: 'HeaderCom',
   components: {
@@ -114,7 +115,6 @@ export default {
       amountVal: 0.01,
       orderVal: '',
       tokenChoose: '1',
-      ethBalance: 0,
       web3: null,
       showConnect: false,
       nav: [
@@ -196,13 +196,18 @@ export default {
     // this.connect();
     console.log(123123);
   },
+  computed: {
+    ethBalance() {
+      const headerStore = useHeaderStore();
+      return headerStore.balance;
+    },
+  },
   methods: {
     async getTheUserBalanceInfo() {
       const headerStore = useHeaderStore();
       await headerStore.getTheUserBalanceApi();
       // let res = await getTheUserBalance();
       // this.ethBalance = res.data[0].balance;
-      this.ethBalance = headerStore.balance;
     },
     async login() {
       const _that = this;
@@ -239,7 +244,6 @@ export default {
       let web3 = new Web3(window.ethereum);
       const _that = this;
       let ethereum = window.ethereum;
-      console.log(123123);
       if (typeof ethereum === 'undefined') {
         //没安装MetaMask钱包进行弹框提示
         alert('请安装MetaMask');
@@ -270,11 +274,13 @@ export default {
             window.web3 = web3;
             _that.web3 = web3;
             _that.conncectAddress = accounts[0];
-            _that.ethBalance = new BigNumber(
+            const headerStore = useHeaderStore();
+            const _ethBalance = new BigNumber(
               await _that.web3.eth.getBalance(accounts[0])
             )
               .div(1e18)
               .toFixed(4);
+            headerStore.setBalance(_ethBalance);
             _that.login();
             //这里返回用户钱包地址
             // callback(accounts[0]);
@@ -320,7 +326,6 @@ export default {
           });
         return;
       }
-
       await transferContract.methods
         .transferETH(amount, receiver, orderId)
         .send({
@@ -329,7 +334,6 @@ export default {
           value: amount,
         });
     },
-
     goTo(page = 'home') {
       this.$router.push({ path: `/${page}` });
     },
@@ -338,237 +342,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-$header-height: 64px;
-.header {
-  // position: fixed;
-  // top: 0;
-  // left: 0;
-  width: 100%;
-  // z-index: 10;
-}
-.header-main {
-  height: $header-height;
-  display: flex;
-  align-content: center;
-  align-items: center;
-  justify-content: space-between;
-  justify-items: center;
-}
-.header-logo {
-  max-height: $header-height - 10px;
-  cursor: pointer;
-}
-.header-nav-text {
-  height: 20px;
-  font-size: 18px;
-  font-weight: normal;
-  font-stretch: normal;
-  font-style: normal;
-  line-height: 1.11;
-  letter-spacing: normal;
-  color: #fff;
-}
-.header-left,
-.header-nav {
-  display: flex;
-  align-items: center;
-  align-content: center;
-  justify-items: center;
-  justify-content: space-between;
-}
-.header-nav {
-  margin-left: 90px;
-}
-.header-nav-text {
-  margin-right: 24px;
-  cursor: pointer;
-  &:last-child {
-    margin-right: 0;
-  }
-}
-.header-button {
-  width: 140px;
-  height: 40px;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  line-height: 40px;
-  border-radius: 20px;
-  border: solid 2px #fff;
-  background-image: linear-gradient(0, #b164f6 15%, #026aa1 58%, #42e2f0 79%);
-  color: #fff;
-  font-weight: 500;
-  cursor: pointer;
-}
-.header-wallet {
-  width: 171px;
-  height: 48px;
-  border-radius: 24px;
-  background-color: #2e0d4a;
-  display: flex;
-  align-items: center;
-  align-content: center;
-  justify-items: center;
-  justify-content: space-around;
-}
-.header-wallet-img {
-  width: 24px;
-  height: 24px;
-}
-.header-wallet-money {
-  font-size: 24px;
-  color: #fff;
-}
-.header-wallet-add {
-  display: inline-block;
-  width: 52px;
-  height: 40px;
-  line-height: 40px;
-  border-radius: 20px;
-  background: url('@/assets/img/headerFooter/icon-add.png') no-repeat center
-    center #a896b5;
-}
-.boxes-button {
-  cursor: pointer;
-  position: relative;
-  overflow: visible;
-  height: 40px;
-  margin-top: 0;
-  margin-left: 16px;
-  &:hover {
-    .header-user {
-      transform: scaleY(1);
-      opacity: 1;
-    }
-    .header-user-down {
-      transform: rotate(0);
-    }
-  }
-}
-.header-user {
-  transform: scaleY(0);
-  position: absolute;
-  top: 100%;
-  left: 0;
-  padding-top: 16px;
-  width: 100%;
-  overflow: hidden;
-  opacity: 0;
-  transition: transform 0.2s ease;
-  transform-origin: top;
-  z-index: 100;
-}
-.header-user-main {
-  padding: 0;
-  border-radius: 12px;
-  border-style: solid;
-  border-width: 2px;
-  border-image-source: linear-gradient(
-    to bottom,
-    #6ce1f9,
-    #00669b 48%,
-    #b770f4
-  );
-  border-image-slice: 1;
-  background-image: linear-gradient(to bottom, #2d1942, #2d1942),
-    linear-gradient(to bottom, #6ce1f9, #00669b 48%, #b770f4);
-  background-origin: border-box;
-  background-clip: content-box, border-box;
-}
-.header-user-content {
-  padding: 8px;
-}
-.header-user-list {
-  font-size: 18px;
-  color: #a896b5;
-  border-radius: 8px;
-  padding: 0 8px;
-  height: 40px;
-  text-align: left;
-  display: flex;
-  align-content: center;
-  align-items: center;
-  &:hover {
-    color: #f5e1d6;
-    background-color: rgba(168, 150, 181, 0.1);
-  }
-}
-.header-user-img {
-  width: 24px;
-  height: 24px;
-}
-.boxes-button-text {
-  max-width: 100px;
-  height: 22px;
-  margin: 0 8px;
-}
-.header-user-down {
-  transform: rotate(180deg);
-  transition: transform 0.2s;
-}
-.header-user-list-img {
-  display: inline-block;
-  width: 24px;
-  height: 24px;
-  margin-right: 8px;
-  background-repeat: no-repeat;
-  background-size: 100% 100%;
-}
-.header-user-list {
-  &:hover {
-    .myProfile {
-      background-image: url('@/assets/img/headerFooter/icon-profile-hover.png');
-    }
-    .wallet {
-      background-image: url('@/assets/img/headerFooter/icon-walletdefault-hover.png');
-    }
-    .walletLog {
-      background-image: url('@/assets/img/headerFooter/icon-walletlogdefault-hover.png');
-    }
-    .competitions {
-      background-image: url('@/assets/img/headerFooter/icon-competitionsdefault-hover.png');
-    }
-    .myCollections {
-      background-image: url('@/assets/img/headerFooter/icon-collectiondefault-hover.png');
-    }
-    .create {
-      background-image: url('@/assets/img/headerFooter/icon-create-hover.png');
-    }
-    .referral {
-      background-image: url('@/assets/img/headerFooter/icon-referraldefault-hover.png');
-    }
-    .settings {
-      background-image: url('@/assets/img/headerFooter/icon-settingsdefault-hover.png');
-    }
-    .logout {
-      background-image: url('@/assets/img/headerFooter/icon-logoutdefault-hover.png');
-    }
-  }
-}
-.myProfile {
-  background-image: url('@/assets/img/headerFooter/icon-profile.png');
-}
-.wallet {
-  background-image: url('@/assets/img/headerFooter/icon-walletdefault.png');
-}
-.walletLog {
-  background-image: url('@/assets/img/headerFooter/icon-walletlogdefault.png');
-}
-.competitions {
-  background-image: url('@/assets/img/headerFooter/icon-competitionsdefault.png');
-}
-.myCollections {
-  background-image: url('@/assets/img/headerFooter/icon-collectiondefault.png');
-}
-.create {
-  background-image: url('@/assets/img/headerFooter/icon-create.png');
-}
-.referral {
-  background-image: url('@/assets/img/headerFooter/icon-referraldefault.png');
-}
-.settings {
-  background-image: url('@/assets/img/headerFooter/icon-settingsdefault.png');
-}
-.logout {
-  background-image: url('@/assets/img/headerFooter/icon-logoutdefault.png');
-}
+@import url('./index.scss');
 </style>

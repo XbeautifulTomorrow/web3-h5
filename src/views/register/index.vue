8fc4d9c1a5c94b4a7fc2d015d5a8d57455637527
Cyclone <15927488117@163.com>
2023-06-09 15:16:56 +0800

    fix:login

--- src/views/register/index.vue
+++ src/views/register/index.vue
@@ -179,11 +179,12 @@ const registerFun = async (formEl) => {
 
       const res = await getReg(data);
       if (res && res.code === 200) {
+        userStore.setLogin(res.data);
         userStore.setReg(res.data);
         if (res.data.certificate) {
           localStorage.setItem("certificate", res.data.certificate);
         }
-        emit("changeTypeFun", "login");
+        closeDialogFun();
       }
     } else {
       console.log("error submit!", fields);

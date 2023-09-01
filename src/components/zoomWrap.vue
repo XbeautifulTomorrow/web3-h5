080b4cadec6f8d5dd4954dc704fddbb56063d71b
sophiayk <857727446@qq.com>
2023-09-01 18:06:09 +0800

    fix:fit

--- src/components/zoomWrap.vue
+++ src/components/zoomWrap.vue
@@ -1,37 +1,25 @@
 <script setup>
 import { ref, reactive, onMounted, onUnmounted } from "vue";
-let designWidth = 1140;
+const designWidth = 1140;
 const designHeight = 1080 - 200;
-const content = ref(null);
-const translateY = ref(null);
+const wrapRef = ref(null);
 const state = reactive({
   ratio: 1,
   origin: "center",
 });
 
 const onResize = () => {
-  const { innerWidth, innerHeight } = window;
-  const contentHeight = content.value.offsetHeight;
-  if (contentHeight > innerHeight) {
-    translateY.value = innerHeight / innerWidth > 0.5 ? 50 : (innerHeight / innerWidth) * 100;
-  } else {
-    translateY.value = 50;
-  }
-  if (innerWidth < 950) {
-    designWidth = innerWidth;
-  }
-  const innerHeights = innerHeight - innerWidth * 0.1;
+  const { innerWidth } = window;
+  const innerHeight = window.innerHeight - window.innerWidth * 0.08;
   const xRatio = innerWidth / designWidth;
-  const yRatio = innerHeights / designHeight;
-  const ratio = innerWidth < 950 ? xRatio : Math.min(xRatio, yRatio);
+  const yRatio = innerHeight / designHeight;
+  const ratio = Math.min(xRatio, yRatio);
   state.ratio = ratio;
   state.origin = xRatio > yRatio ? "center" : "top";
 };
 
 onMounted(() => {
-  setTimeout(() => {
-    onResize();
-  }, 200);
+  onResize();
   window.addEventListener("resize", onResize);
 });
 
@@ -43,7 +31,7 @@ onUnmounted(() => {
 <template>
   <div class="wrap" ref="wrapRef">
     <div class="scale" :style="`transform: scale(${state.ratio});transform-origin: ${state.origin}`">
-      <div class="content" ref="content" :style="{ transform: `translate(-50%, -${translateY}%)` }">
+      <div class="content">
         <slot />
       </div>
     </div>
@@ -67,7 +55,7 @@ onUnmounted(() => {
   width: 98vw;
   transform: translate(-50%, -50%);
 }
-@media (min-width: 950px) and (max-width: 1650px) {
+@media screen and (max-width: 1530px) {
   .scale {
     transform: scale(1) !important;
   }

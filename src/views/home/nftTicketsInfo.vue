e251c5b1af0d2248871165aa03ea7f3f5debcfd3
Cyclone <15927488117@163.com>
2023-09-08 20:26:53 +0800

    fix:tickets info

--- src/views/home/nftTicketsInfo.vue
+++ src/views/home/nftTicketsInfo.vue
@@ -36,7 +36,10 @@
         </div>
         <div class="nft_details_r_bg border_bg">
           <div class="nft_details_r">
-            <div class="nft_name text-ellipsis">{{ `${nftInfo && nftInfo.name} #${nftInfo && nftInfo.tokenId}` }}</div>
+            <div class="nft_name text-ellipsis">
+              <span>{{ nftInfo && nftInfo.name }}</span>
+              <span v-if="formatSeries(nftInfo)">{{ ` #${nftInfo && nftInfo.tokenId}` }}</span>
+            </div>
             <div class="nft_activity">
               <div class="price_box">
                 <div class="price">
@@ -1067,6 +1070,13 @@ export default {
         },
         series: series,
       };
+    },
+    formatSeries(event) {
+      if (!event) return false;
+      const { name, tokenId } = event;
+      if (!name || !tokenId) return false;
+      const isShow = name.indexOf(tokenId) > -1;
+      return !isShow
     }
   },
   watch: {

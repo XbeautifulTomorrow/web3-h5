8506396f8c6ce39fde613fac7019387ee297672e
sophiayk <857727446@qq.com>
2023-09-14 09:08:07 +0800

    fix: chain

--- src/views/home/nftTicketsInfo.vue
+++ src/views/home/nftTicketsInfo.vue
@@ -884,7 +884,15 @@ export default {
         chainLink = process.env.VUE_APP_CHAIN_OKT_TEST_ADDR;
       } else if (event.chainType == "BSC_TEST") {
         chainLink = process.env.VUE_APP_CHAIN_BSC_TEST_ADDR;
+      } else if (event.chainType == "BASE") {
+        chainLink = process.env.VUE_APP_CHAIN_BASE_ADDR;
+      } else if (event.chainType == "BSC") {
+        chainLink = process.env.VUE_APP_CHAIN_BSC_ADDR;
+      } else if (event.chainType == "OKT") {
+        chainLink = process.env.VUE_APP_CHAIN_OKT_ADDR;
       }
+
+
 
       openUrl(`${chainLink}${event.txid}`);
     },

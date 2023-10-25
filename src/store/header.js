31054280c32bb4f0886319ac0a7f9fac575c46d1
sophiayk <857727446@qq.com>
2023-10-25 14:23:59 +0800

    fix:dataLayer

--- src/store/header.js
+++ src/store/header.js
@@ -11,21 +11,31 @@ export const useHeaderStore = defineStore("headerStore", {
       walletNftSystemStatus: false,
       oneNftStatus: false,
       enteredStatus: false,
-      myTreasureDrawStatus: false
+      myTreasureDrawStatus: false,
     },
-    walletAddr: ""
+    walletAddr: "",
+    userRechargeShowList: [],
   }),
   persist: {
     enabled: true,
-    strategies: [
-      { key: "balance", storage: sessionStorage, paths: ["balance"] },
-    ],
+    strategies: [{ key: "balance", storage: sessionStorage, paths: ["balance"] }],
   },
   actions: {
     async getTheUserBalanceApi(params) {
       const res = await getTheUserBalance(params);
       if (res && res.data) {
-        this.balance = res.data[0].balance;
+        this.balance = res.data.balanceList[0].balance;
+        this.userRechargeShowList = res.data.userRechargeShowList;
+        let totalPrice = this.userRechargeShowList.reduce((sum, item) => sum + item.price, 0);
+        try {
+          // eslint-disable-next-line no-undef
+          dataLayer.push({
+            event: "top_up_status",
+            price: totalPrice,
+          });
+        } catch (err) {
+          console.log(err);
+        }
       }
     },
     // 积分余额
@@ -39,7 +49,7 @@ export const useHeaderStore = defineStore("headerStore", {
     async fetchGlobalNew() {
       const res = await getGlobalNew();
       if (res && res.data) {
-        this.newStatus = res.data
+        this.newStatus = res.data;
       }
     },
     setBalance(data) {

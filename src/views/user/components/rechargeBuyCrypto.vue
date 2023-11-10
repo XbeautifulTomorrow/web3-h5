5d6501aee9e48e232e7e194af435bde96ede5039
sophiayk <857727446@qq.com>
2023-11-10 18:08:02 +0800

    feat:buy crypto

--- src/views/user/components/rechargeBuyCrypto.vue
+++ src/views/user/components/rechargeBuyCrypto.vue
@@ -4,15 +4,11 @@
       <div class="withdraw_item">
         <div class="withdraw_item_lable exchange_item_lable">
           <p>
-            <span> {{ $t("user.from") }} </span> <span class="required">*</span>
+            <span> {{ $t("YOU PAY WITH") }} </span> <span class="required">*</span>
           </p>
         </div>
         <div class="withdraw_addr_input exchange_addr_input">
-          <div class="exhange_icon" v-if="exchangeOperating == 1">
-            <img :src="getCion('USDT')" alt="" />
-            <span>USDT</span>
-          </div>
-          <div class="exhange_icon" v-else>
+          <div class="exhange_icon">
             <el-select v-model="exchangeInfo.exchangeFromCoin" @change="fetchExchangeRate" :popper-append-to-body="false">
               <template #prefix>
                 <img :src="getCion(exchangeInfo.exchangeFromCoin)" alt="" />
@@ -36,10 +32,73 @@
         </div>
         <div class="withdraw_item_error">
           <p>{{ exchangeAmountTips }}</p>
-          <p class="max" @click="maxFunc">MAX</p>
+        </div>
+      </div>
+      <div class="withdraw_item">
+        <div class="withdraw_item_lable exchange_item_lable">
+          <p>
+            <span> {{ $t("YOU GET") }} </span> <span class="required">*</span>
+          </p>
+        </div>
+        <div class="withdraw_addr_input exchange_addr_input">
+          <div class="exhange_icon">
+            <img :src="getCion('USDT')" alt="" />
+            <span>USDT</span>
+          </div>
+          <el-input
+            type="number"
+            v-model="exchangeInfo.exchangeFromAmount"
+            placeholder="0.0000"
+            @input="onVerifyExchange('amount')"
+          ></el-input>
+        </div>
+        <div class="withdraw_item_error">
+          <p>{{ exchangeAmountTips }}</p>
         </div>
       </div>
     </div>
+    <div class="module_info">
+      <div class="item_info">
+        <p class="label">{{ $t("Payment Provider") }}</p>
+        <p class="info_tag">{{ $t("user.zeroFee") }}</p>
+      </div>
+      <div class="item_info">
+        <p class="label">{{ $t("user.exchangeRate") }}</p>
+        <p class="info">
+          {{ $t("xxxxxxx") }}
+        </p>
+      </div>
+    </div>
+    <div class="module_info">
+      <div class="item_info">
+        <p class="label">{{ $t("Total（including fee）") }}</p>
+        <p class="info font1">100 USD</p>
+      </div>
+      <div class="item_info">
+        <p class="label">{{ $t("You Will Get") }}</p>
+        <p class="info font2">98.54 USDT</p>
+      </div>
+    </div>
+    <div class="notice_box">
+      <span class="label">NOTICE:</span>
+      <span class="tip">Depending on the blockchain, the deposit may take a few minutes to 1 hour to arrive.</span>
+    </div>
+    <div class="form-rember">
+      <span class="form-rember-rectangle" @click="showRememberFun">
+        <span v-show="showRemember" class="form-rember-rectangle-fill"></span>
+      </span>
+      <div class="form-rember-text">
+        <el-tooltip
+          class="item"
+          effect="dark"
+          content="Disclaimer: The above third party services can be used to purchase crypto that can be used to play on Bitzing. By registering on their platform, you are also accepting to their terms of service and will be required to pass their KYC process, which runs independently to ours."
+          placement="top"
+        >
+          <p>{{ $t("I have read and agree to the disclaimer.") }}</p>
+        </el-tooltip>
+      </div>
+    </div>
+    <div class="handle_btn" @click="submitFunc">第三方支付的名称</div>
   </div>
 </template>
 <script>
@@ -54,6 +113,7 @@ export default {
   props: {},
   data() {
     return {
+      showRemember: false,
       exchangeInfo: {
         exchangeFromAmount: null,
         exchangeToAmount: null,
@@ -73,6 +133,10 @@ export default {
       const headerStore = useUserStore();
       return headerStore.getCoin(coin);
     },
+    showRememberFun() {
+      this.showRemember = !this.showRemember;
+    },
+    submitFunc() {},
   },
   mounted() {},
   created() {},
@@ -80,7 +144,131 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
+@import "./wallet.scss";
 .buy_crypto_box {
+  .module_info {
+    padding: 1.25rem;
+    border-radius: 0.625rem;
+    background-color: #191c28;
+    margin-top: 1.25rem;
+    .item_info {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      margin-bottom: 1.25rem;
+      &:last-child {
+        margin-bottom: 0;
+      }
+      .label {
+        font-size: 0.875rem;
+        color: #a9a4b4;
+      }
+      .info_tag {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        padding: 4px 16px;
+        border-radius: 4px;
+        background-color: rgba(12, 187, 62, 0.3);
+        color: #0cbb3e;
+        font-size: 0.875rem;
+      }
+      .info {
+        display: flex;
+        align-items: center;
+        color: #ffffff;
+        &.font1 {
+          font-family: Inter;
+          font-size: 1.5rem;
+          font-weight: normal;
+        }
+        &.font2 {
+          font-family: Inter;
+          font-size: 1.5rem;
+          font-weight: bold;
+        }
+        .coin {
+          width: 1.5rem;
+          height: 1.5rem;
+          margin-left: 0.5rem;
+        }
+      }
+    }
+  }
+  .notice_box {
+    height: 3.75rem;
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: center;
+    gap: 0.25rem;
+    padding: 0 1.25rem;
+    border-radius: 0.625rem;
+    background-color: #332e1b;
+    margin-top: 1.25rem;
+    .label {
+      color: #fad54d;
+      font-weight: bold;
+    }
+    .tip {
+      color: #a9a4b4;
+    }
+  }
+  .form-rember {
+    display: flex;
+    align-items: center;
+    margin-top: 0.625rem;
+    margin-bottom: 1.875rem;
+  }
+
+  .form-rember-rectangle {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 1rem;
+    height: 1rem;
+    margin-right: 0.75rem;
+    border-radius: 0.1875rem;
+    border: solid 1px #a9a4b4;
+    cursor: pointer;
+  }
+
+  .form-rember-rectangle-fill {
+    display: inline-block;
+    width: 0.5rem;
+    height: 0.5rem;
+    border-radius: 0.125rem;
+    background-color: #fad54d;
+  }
+
+  .form-rember-text {
+    font-size: 1rem;
+    line-height: 1.6;
+    text-align: left;
+    color: #a9a4b4;
+  }
+
+  .handle_btn {
+    width: 100%;
+    height: 3.75rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 0.5rem;
+    background-image: linear-gradient(to top, #e3a406, #fbd851);
+    cursor: pointer;
+    font-size: 1.125rem;
+    line-height: 1.21;
+    text-align: center;
+    color: #13151f;
+    &.dark_btn {
+      border: solid 1px rgba(228, 231, 245, 0.15);
+      background-color: #292c36;
+      background-image: none;
+      color: #e4e7f5;
+    }
+  }
 }
 
 .content {
@@ -191,26 +379,6 @@ export default {
   }
 }
 
-.handle_btn {
-  width: 100%;
-  height: 3.75rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-radius: 0.5rem;
-  background-image: linear-gradient(to top, #e3a406, #fbd851);
-  cursor: pointer;
-  font-size: 1.125rem;
-  line-height: 1.21;
-  text-align: center;
-  color: #13151f;
-  &.dark_btn {
-    border: solid 1px rgba(228, 231, 245, 0.15);
-    background-color: #292c36;
-    background-image: none;
-    color: #e4e7f5;
-  }
-}
 .status_icon {
   margin-bottom: 1.875rem;
 }
